4f23474881756509d95b0ac58338e2905e9dd27b
Sjaarda Jennifer Lynn <je4649@intranet.chuv>
2020-01-15 13:48:14 +0100

    add rscript to reupdate Neale SGG directory

--- code/process_Neale.sh
+++ code/process_Neale.sh
@@ -4,6 +4,9 @@
 #SBATCH --job-name process_Neale
 #SBATCH --output analysis/process_Neale.out
 
+Rscript ${SGG_generic}/scripts/merge_Neale.r
+### reupdate Neale_SGG_dir using ${SGG_generic}/scripts/merge_Neale.r
+
 process_IV_extractions=$(sbatch $SGG_generic/scripts/UKBB/gen_rslurm_Neale_extraction.sh | cut -f 4 -d' ')
 until [[ -f ${SGG_generic}/_rslurm_Neale_extraction/submit.sh ]];
 do
@@ -12,5 +15,3 @@ done
 get_Neale_IV=$(sbatch --dependency=afterok:$process_IV_extractions ${SGG_generic}/_rslurm_Neale_extraction/submit.sh | cut -f 4 -d' ')
 
 sbatch --dependency=afterok:$get_Neale_IV ${SGG_generic}/scripts/UKBB/MAKE_Neale_IVs_clump.sh
-
-### reupdate Neale_SGG_dir using ${SGG_generic}/scripts/merge_Neale.r
